7579a3f6084e55ed53dd24d6111e1fa43d17d489
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-10-22 14:08:37 +0530

    Create ItemInfoDTO class

--- StoreManagementSystem/src/main/java/controller/ItemInfoController.java
+++ StoreManagementSystem/src/main/java/controller/ItemInfoController.java
@@ -1,4 +1,64 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ItemInfoController {
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colQtyOnHand;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private TableView<?> tblItemInfo;
+
+    @FXML
+    private TextField txtCategory;
+
+    @FXML
+    private TextField txtDescription;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtQtyOnHand;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
